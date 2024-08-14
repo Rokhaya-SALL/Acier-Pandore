4acec0f2f6a8191d2b47fed135ede579f938bc7c
Rokhaya SALL <sall.nrd69@gmail.com>
2024-08-14 18:34:50 +0200

    Ajout des images

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -22,23 +22,22 @@ class AppFixtures extends Fixture
         }
 
         $products = [
-            ['Bracelet en acier inoxydable', 'Un bracelet élégant en acier inoxydable de haute qualité.', 49, 'bijoux19.jpg', 'Bracelet'],
-            ['Collier en acier inoxydable', 'Un collier raffiné en acier inoxydable, parfait pour toutes les occasions.', 69, 'bijoux18.jpg', 'Collier'],
-            ['Boucles d\'oreilles en acier inoxydable', 'Des boucles d\'oreilles en acier inoxydable, résistantes et brillantes.', 29, 'bijoux8.jpg', 'Boucle d\'oreille'],
-            ['Boucles d\'oreilles en acier inoxydable', 'Des boucles d\'oreilles en acier inoxydable, résistantes et brillantes.', 29, 'bijoux10.jpg', 'Boucle d\'oreille'],
-            ['Boucles d\'oreilles en acier inoxydable', 'Des boucles d\'oreilles en acier inoxydable, résistantes et brillantes.', 29, 'bijoux15.jpg', 'Boucle d\'oreille'],
-            ['Boucles d\'oreilles en acier inoxydable', 'Des boucles d\'oreilles en acier inoxydable, résistantes et brillantes.', 29, 'bijoux16.jpg', 'Boucle d\'oreille'],
-            ['Boucles d\'oreilles en acier inoxydable', 'Des boucles d\'oreilles en acier inoxydable, résistantes et brillantes.', 29, 'bijoux17.jpg', 'Boucle d\'oreille'],
-            ['Bagues en acier inoxydable', 'Lot de 3 bagues en acier inoxydable avec un design intemporel.', 39, 'bijoux1.jpg', 'Bague'],
-            ['Bagues en acier inoxydable', 'Lot de 3 bagues en acier inoxydable avec un design intemporel.', 39, 'bijoux2.jpg', 'Bague'],
-            ['Bagues en acier inoxydable', 'Lot de 4 bagues en acier inoxydable avec un design intemporel.', 39, 'bijoux3.jpg', 'Bague'],
-            ['Bagues en acier inoxydable', 'Lot de 2 bagues en acier inoxydable avec un design intemporel.', 39, 'bijoux4.jpg', 'Bague'],
-            ['Bague en acier inoxydable', 'Une bague en acier inoxydable avec un design intemporel.', 39, 'bijoux5.jpg', 'Bague'],
-            ['Bagues en acier inoxydable', 'Lot de 3 bagues en acier inoxydable avec un design intemporel.', 39, 'bijoux6.jpg', 'Bague'],
-            ['Bague en acier inoxydable', 'Une bague en acier inoxydable avec un design intemporel.', 39, 'bijoux7.jpg', 'Bague'],
-            ['Bague en acier inoxydable', 'Une bague en acier inoxydable avec un design intemporel.', 39, 'bijoux9.jpg', 'Bague'],
-            ['Bagues en acier inoxydable', 'Lot de 4 bagues en acier inoxydable avec un design intemporel.', 39, 'bijoux14.jpg', 'Bague'],
-            
+            ['Bracelet en acier inoxydable', 'Un bracelet élégant en acier inoxydable de haute qualité.', 49, 'images/bijoux19.jpg', 'Bracelet'],
+            ['Collier en acier inoxydable', 'Un collier raffiné en acier inoxydable, parfait pour toutes les occasions.', 69, 'images/bijoux18.jpg', 'Collier'],
+            ['Boucles d\'oreilles en acier inoxydable', 'Des boucles d\'oreilles en acier inoxydable, résistantes et brillantes.', 29, 'images/bijoux8.jpg', 'Boucle d\'oreille'],
+            ['Boucles d\'oreilles en acier inoxydable', 'Des boucles d\'oreilles en acier inoxydable, résistantes et brillantes.', 29, 'images/bijoux10.jpg', 'Boucle d\'oreille'],
+            ['Boucles d\'oreilles en acier inoxydable', 'Des boucles d\'oreilles en acier inoxydable, résistantes et brillantes.', 29, 'images/bijoux15.jpg', 'Boucle d\'oreille'],
+            ['Boucles d\'oreilles en acier inoxydable', 'Des boucles d\'oreilles en acier inoxydable, résistantes et brillantes.', 29, 'images/bijoux16.jpg', 'Boucle d\'oreille'],
+            ['Boucles d\'oreilles en acier inoxydable', 'Des boucles d\'oreilles en acier inoxydable, résistantes et brillantes.', 29, 'images/bijoux17.jpg', 'Boucle d\'oreille'],
+            ['Bagues en acier inoxydable', 'Lot de 3 bagues en acier inoxydable avec un design intemporel.', 39, 'images/bijoux1.jpg', 'Bague'],
+            ['Bagues en acier inoxydable', 'Lot de 3 bagues en acier inoxydable avec un design intemporel.', 39, 'images/bijoux2.jpg', 'Bague'],
+            ['Bagues en acier inoxydable', 'Lot de 4 bagues en acier inoxydable avec un design intemporel.', 39, 'images/bijoux3.jpg', 'Bague'],
+            ['Bagues en acier inoxydable', 'Lot de 2 bagues en acier inoxydable avec un design intemporel.', 39, 'images/bijoux4.jpg', 'Bague'],
+            ['Bague en acier inoxydable', 'Une bague en acier inoxydable avec un design intemporel.', 39, 'images/bijoux5.jpg', 'Bague'],
+            ['Bagues en acier inoxydable', 'Lot de 3 bagues en acier inoxydable avec un design intemporel.', 39, 'images/bijoux6.jpg', 'Bague'],
+            ['Bague en acier inoxydable', 'Une bague en acier inoxydable avec un design intemporel.', 39, 'images/bijoux7.jpg', 'Bague'],
+            ['Bague en acier inoxydable', 'Une bague en acier inoxydable avec un design intemporel.', 39, 'images/bijoux9.jpg', 'Bague'],
+            ['Bagues en acier inoxydable', 'Lot de 4 bagues en acier inoxydable avec un design intemporel.', 39, 'images/bijoux14.jpg', 'Bague'],
         ];
 
         foreach ($products as $data) {
@@ -48,7 +47,6 @@ class AppFixtures extends Fixture
                     ->setPrice($data[2])
                     ->setImage($data[3]);
 
-            
             foreach ($categories as $category) {
                 if ($category->getName() === $data[4]) {
                     $product->setCategory($category);
