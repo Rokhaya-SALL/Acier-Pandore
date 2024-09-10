6090dc26df4c40e4ae74986203363db6f733bce6
Rokhaya SALL <sall.nrd69@gmail.com>
2024-09-10 04:35:01 +0200

    Migrations

--- migrations/Version20240910014035.php
+++ migrations/Version20240910014035.php
@@ -19,7 +19,7 @@ final class Version20240910014035 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // Ajouter les nouvelles colonnes et renommer 'adresse' en 'address'
+        // Add new columns and the 'address' column
         $this->addSql('ALTER TABLE user 
             ADD first_name VARCHAR(100) NOT NULL, 
             ADD last_name VARCHAR(100) NOT NULL, 
@@ -27,13 +27,13 @@ final class Version20240910014035 extends AbstractMigration
             ADD postal_code VARCHAR(20) DEFAULT NULL, 
             ADD country VARCHAR(100) DEFAULT NULL, 
             ADD phone VARCHAR(20) DEFAULT NULL, 
-            CHANGE adresse address VARCHAR(255) DEFAULT NULL'
+            ADD address VARCHAR(255) DEFAULT NULL'
         );
     }
 
     public function down(Schema $schema): void
     {
-        // Supprimer les nouvelles colonnes et restaurer les anciennes
+        // Remove the new columns and the 'address' column
         $this->addSql('ALTER TABLE user 
             DROP first_name, 
             DROP last_name, 
@@ -41,7 +41,7 @@ final class Version20240910014035 extends AbstractMigration
             DROP postal_code, 
             DROP country, 
             DROP phone, 
-            CHANGE address adresse VARCHAR(255) DEFAULT NULL'
+            DROP address'
         );
     }
 }
