f1bb7d07176485fe677b09b6022d36381525c6b2
Rokhaya SALL <sall.nrd69@gmail.com>
2024-09-10 04:15:54 +0200

    Migrations

--- migrations/Version20240910014035.php
+++ migrations/Version20240910014035.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
 /**
- * Auto-generated Migration: Please modify to your needs!
+ * Auto-generated Migration: Please modify it to your needs!
  */
 final class Version20240910014035 extends AbstractMigration
 {
@@ -19,13 +19,29 @@ final class Version20240910014035 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user ADD first_name VARCHAR(100) NOT NULL, ADD last_name VARCHAR(100) NOT NULL, ADD city VARCHAR(100) DEFAULT NULL, ADD postal_code VARCHAR(20) DEFAULT NULL, ADD country VARCHAR(100) DEFAULT NULL, ADD phone VARCHAR(20) DEFAULT NULL, DROP prenom, DROP nom, DROP ville, DROP code_postal, DROP pays, DROP telephone, CHANGE adresse address VARCHAR(255) DEFAULT NULL');
+        // Ajouter les nouvelles colonnes et renommer 'adresse' en 'address'
+        $this->addSql('ALTER TABLE user 
+            ADD first_name VARCHAR(100) NOT NULL, 
+            ADD last_name VARCHAR(100) NOT NULL, 
+            ADD city VARCHAR(100) DEFAULT NULL, 
+            ADD postal_code VARCHAR(20) DEFAULT NULL, 
+            ADD country VARCHAR(100) DEFAULT NULL, 
+            ADD phone VARCHAR(20) DEFAULT NULL, 
+            CHANGE adresse address VARCHAR(255) DEFAULT NULL'
+        );
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user ADD prenom VARCHAR(100) NOT NULL, ADD nom VARCHAR(100) NOT NULL, ADD ville VARCHAR(100) DEFAULT NULL, ADD code_postal VARCHAR(20) DEFAULT NULL, ADD pays VARCHAR(100) DEFAULT NULL, ADD telephone VARCHAR(20) DEFAULT NULL, DROP first_name, DROP last_name, DROP city, DROP postal_code, DROP country, DROP phone, CHANGE address adresse VARCHAR(255) DEFAULT NULL');
+        // Supprimer les nouvelles colonnes et restaurer les anciennes
+        $this->addSql('ALTER TABLE user 
+            DROP first_name, 
+            DROP last_name, 
+            DROP city, 
+            DROP postal_code, 
+            DROP country, 
+            DROP phone, 
+            CHANGE address adresse VARCHAR(255) DEFAULT NULL'
+        );
     }
 }
